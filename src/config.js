b91785665e5dba478684d25edaeb30f99b8e3bb8
Xander <x20297782@gmail.com>
2025-04-08 13:25:00 +0800

    feat: update topic

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 const API_BASE_URL = "https://ziopwmhohqon.usw.sealos.io/api/v1";
+// const API_BASE_URL = "http://localhost:5001/api/v1";
 
 export const ENDPOINTS = {
     GET_TOPICS: `${API_BASE_URL}/topic/getTopics`,
+    UPDATE_TOPIC: `${API_BASE_URL}/topic/updateTopic`,
     GET_COMMENTS: (topicId) => `${API_BASE_URL}/topic/getComments/${topicId}`,
     POST_COMMENT: `${API_BASE_URL}/topic/comment`,
     GET_TOPIC_BY_ID: (topicId) => `${API_BASE_URL}/topic/getTopic/${topicId}`,
